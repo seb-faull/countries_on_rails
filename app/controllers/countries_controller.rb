338cede36e1865ebf41245fb7f6bd49b5abe31b3
seb-faull <seb_faull@hotmail.co.uk>
2017-10-02 22:34:16 +0100

    adding 7 restful standards

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,23 +1,48 @@
 class CountriesController < ApplicationController
 
+  #Index
   def index
+    @countries = Country.all
   end
 
+
+  #Show
   def show
+    @country = Country.find(params[:id])
   end
 
+
+  #Edit
   def edit
+    @country = Country.find(params[:id])
   end
 
+
+  #Create
   def create
+    new_country = Country.new
+    new_country.name = params[:name]
+    new_country.population = params[:population]
+    new_country.flag_url = params[:flag_url]
+    new_country.language = params[:language]
+    new_country.leader = params[:leader]
+    new_country.save
+
+    redirect_to "/countries"
   end
 
+
+  #New
   def new
   end
 
+
+  #Update
   def update
   end
 
+
+  #Delete
   def destroy
   end
   
